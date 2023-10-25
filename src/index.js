79648f971e2da26f831ae7bd0880436203279425
mlr <leraymathieu@gmail.com>
2023-10-25 21:49:32 +0200

    object part

--- src/index.js
+++ src/index.js
@@ -39,3 +39,29 @@ const arr = [1,2,2]
 //Précédence 
 // https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Operators/Operator_precedence
 let a = 2 + 2 * 6 / 2;
+
+const terre = {
+    population: 7e7,
+    satellite: 'Lune',
+    temperature: {
+        min: -70,
+        max:60
+    },
+    isOld:false,
+    //method (fonction qui appartient à un objet)
+    getTemperature: function () {
+        console.log("15");
+    },
+    // équivaut à 
+    getTemperatures() {
+        console.log("15");
+    }
+}
+//dote notation
+console.log(terre.population);
+//crochets
+console.log(terre['satellite']);
+
+const prop = "temperature";
+console.log(terre[prop]);
+terre.getTemperature();
