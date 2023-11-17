e9a03fb16f457d71e2b44971826c43eb9b908c2c
Mathieu Le Ray <leraymathieu@gmail.com>
2023-11-17 16:52:39 +0100

    ajout de la branche this

--- src/index.js
+++ src/index.js
@@ -1,55 +1,49 @@
-// les fonctions sont des objets
-
-// déclaration de fonction
-function func(params) {
-    console.log("func exec");
-}
-func();
-
-//expression de fonction
-const fn = function func2() {
-    console.log("func 2 exec");
-}
-fn();
-//fonction anonyme
-const fn2 = function() {
-    console.log("func 3 exec");
-}
-console.log(fn.name); //func2
-console.log(fn2.name); //fn2
-
-function fnToUp(param = "default") {
-    //avt ES6 param = param || 'default';
-    //depuis ES6 (param = 'default')
-
-
-    console.log(param.toUpperCase());
+const b = {
+    name: "Mathieu",
+    lastname: "Le Ray"
 };
 
-fnToUp("test");
-
-function add(){
-    //console.log(arguments);
-    let total = 0;
-    for (let i = 0; i < arguments.length; i++) {
-       total += arguments[i];
-        
+function bonjour(lang) {
+    if (lang === "fr") {
+        console.log(`bonjour: ${this.name + this.lastname}`);
     }
-    console.log(total);
+    else{
+        console.log(`hello: ${this.name + this.lastname}`);
+    }
+    
+}
+bonjour(b) // undefined
+bonjour.call(b, "fr") // bonjour MAthieuLe ray
+bonjour.apply(b, ["fr"]) // idem mais on passe un array en parametres - bonjour MAthieuLe ray
+
+const c = bonjour.bind(b);
+c("fr");
+
+const a = param => {
+    console.log(param);
 }
 
-add(1,2,3,4);
+const d = (param1, param2) => {
+    console.log(param1);
+    console.log(param2);
+}
+const e = (a, b) => a + b;
 
-function add2(operateur,...numbers){
-    let total = 0;
-    if (operateur === "+") {
-        for (let i = 0; i < numbers.length; i++) {
-            total += numbers[i];
-             
-         }
+console.log(e(1, 2));
+
+// Les closures
+const f = () => {
+    let arr = [];
+    for (var index = 0; index < 3; index++) {
+        arr.push(function() {
+            console.log(index);
+        })
     }
-   
-    console.log(total);
+    return arr;
 }
 
-add2("+",1,2,3)
+const tab = f();
+
+tab[0]();
+tab[1]();
+tab[2]();
